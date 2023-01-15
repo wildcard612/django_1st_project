2db5071e3d2dfccb284e06ee513706b2afd42446
Sebastian Okoniewski <sebastian.okoniewski90@gmail.com>
2023-01-15 21:33:21 +0700

    adding class entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
     def __str__(self):
         ''' model in string'''
         return self.text
+
+class Entry(models.Model):
+    '''info about progress'''
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        '''model in string'''
+        return f'{self.text[:50]}...'
