f01a451f32c65aecf8d80643b4d8b3aa46a63b75
Sebastian Okoniewski <sebastian.okoniewski90@gmail.com>
2023-01-18 00:19:01 +0700

    addeding topic.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     #main site
     path('', views.index, name='index'),
     #all topics
-path('topics/', views.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
+    #single topic detail page
+    path('topics/(<int:topic_id>)/', views.topic, name='topic'),
 ]
